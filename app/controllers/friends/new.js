0765f55dff33d3564b9f6d29a76fab7879228c3d
Adolfo Builes <builes.adolfo@gmail.com>
2016-05-25 09:29:36 -0500

    Add cancel action to friends/new.

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -9,6 +9,7 @@ export default Ember.Controller.extend({
     },
     cancel() {
       console.log('+--- cancel action called in friends new controller');
+      this.transitionToRoute('friends.index');
     }
   }
 });

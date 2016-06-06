fe99c6243c060baec7c5ea039f9294560422b934
Adolfo Builes <builes.adolfo@gmail.com>
2016-06-06 09:55:32 -0500

    Add power-select.

--- app/components/loans/edit-form.js
+++ app/components/loans/edit-form.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //
+  // By default the store is not injected into components, so we use
+  // the "inject.service" helper to make it avaible.
+  //
+  store: Ember.inject.service(),
+  articles: Ember.computed({
+    get() {
+      //
+      // Since we are using Ember.inject.service, we need to call the
+      // store using the get helper
+      //
+      return this.get('store').findAll('article');
+    }
+  }).readOnly()
 });

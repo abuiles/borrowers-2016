9b3c3a4230268cf91ee583862163b75f9dbd73d7
Adolfo Builes <builes.adolfo@gmail.com>
2016-06-07 08:32:28 -0500

    Complete loans.new.

--- app/components/loans/edit-form.js
+++ app/components/loans/edit-form.js
@@ -14,5 +14,26 @@ export default Ember.Component.extend({
       //
       return this.get('store').findAll('article');
     }
-  }).readOnly()
+  }).readOnly(),
+  //
+  // Save and cancel are not declared inside actions key, , we'll be
+  // using it as closure actions.
+  //
+  save() {
+    //
+    // We probably want to verify here that the model has an article
+    // before saving
+    //
+    this.get('model').save().then((model) => {
+      this.back(model.get('friend'));
+    }, () => {
+      this.set(
+        'errorMessage',
+        'there was something wrong saving the loan'
+      );
+    });
+  },
+  cancel() {
+    this.back(this.get('model.friend'));
+  }
 });

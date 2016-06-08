a574b3c22dfe5950dd35692b1a8a89c1846adfcb
Adolfo Builes <builes.adolfo@gmail.com>
2016-06-08 05:15:22 -0500

    Use borrowers-dates.

--- app/utils/date-helpers.js
+++ app/utils/date-helpers.js
@@ -1,5 +1,7 @@
+import { format as borrowersDate } from 'borrowers-dates';
+
 function formatDate(date, format) {
-  return window.moment(date).format(format);
+  return borrowersDate(date, format);
 }
 
 export {
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -41,5 +41,7 @@ module.exports = function(defaults) {
 
   app.import('bower_components/moment/moment.js');
 
+  app.import('bower_components/borrowers-dates/index.js');
+
   return app.toTree();
 };

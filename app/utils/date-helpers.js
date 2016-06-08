4cbc54f8b5cf0760e7729fc5a7d38a7f4ddffb4c
Adolfo Builes <builes.adolfo@gmail.com>
2016-06-08 05:15:40 -0500

    Revert "Use borrowers-dates."
    This reverts commit a574b3c22dfe5950dd35692b1a8a89c1846adfcb.

--- app/utils/date-helpers.js
+++ app/utils/date-helpers.js
@@ -1,7 +1,5 @@
-import { format as borrowersDate } from 'borrowers-dates';
-
 function formatDate(date, format) {
-  return borrowersDate(date, format);
+  return window.moment(date).format(format);
 }
 
 export {
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -41,7 +41,5 @@ module.exports = function(defaults) {
 
   app.import('bower_components/moment/moment.js');
 
-  app.import('bower_components/borrowers-dates/index.js');
-
   return app.toTree();
 };

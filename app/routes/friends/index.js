6cde5e4190a8e57815b380139ff80d32684b569d
Adolfo Builes <builes.adolfo@gmail.com>
2016-06-14 10:09:01 -0500

    Sideload records.

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('friend');
+    //
+    // We use now store.query and pass include in the options
+    //
+    return this.store.query('friend', { include: 'loans,loans.article' });
   }
 });

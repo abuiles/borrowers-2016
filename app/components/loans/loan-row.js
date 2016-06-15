088d5c0f4c0ae2dd223326f749fc6aeaf2d9d972
Adolfo Builes <builes.adolfo@gmail.com>
2016-06-15 09:03:54 -0500

    Create observer.

--- app/components/loans/loan-row.js
+++ app/components/loans/loan-row.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'tr',
-  loan: null // passed-in
+  loan: null, // passed-in
+  stateChanged: Ember.observer('loan.returned', function() {
+    var loan = this.get('loan');
+    console.log('OMG Expensive operation because loan state changed');
+  })
 });

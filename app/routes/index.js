03a7bc3a723749f34cec607beb37180bb5a33b82
Adolfo Builes <builes.adolfo@gmail.com>
2016-06-28 10:22:49 -0500

    Add host.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Route.extend({
   //
@@ -7,7 +8,7 @@ export default Ember.Route.extend({
   //
   ajax: Ember.inject.service(),
   model()  {
-    return this.get('ajax').request('/friends').then(function(data){
+    return this.get('ajax').request(`${config.host}/friends`).then(function(data){
       return {
         friendsCount: data.data.length
       };
--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
+    host: 'http://api.ember-101.com',
     modulePrefix: 'borrowers',
     environment: environment,
     baseURL: '/',
